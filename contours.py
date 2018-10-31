a9e7aafecbcc1fc7649a617c09c14a7945f857ad
Spencer Kraisler <spencerkraisler@gmail.com>
2018-10-30 21:46:31 -0700

    organize code more

--- contours.py
+++ contours.py
@@ -1,6 +1,12 @@
 import cv2
 import numpy as np 
 
+
+
+def getContours(mask):
+	_, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	return contours
+
 THRESH_CONTOUR_AREA = 2000
 
 def threshold_area(contours):
--- fingerpaint.py
+++ fingerpaint.py
@@ -2,35 +2,9 @@ import cv2
 import matplotlib
 import matplotlib.pyplot as plt 
 import numpy as np 
-from contours import handleContours
-
-
-def getHistogram(img):
-	lab_img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-	hist = cv2.calcHist([lab_img], [1, 2], None, 
-		 				[15, 15], [0, 255, 0, 255])
-	cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
-	#hist = dilate(hist, DILATION_KERNEL_SIZE)
-	return hist
-
-def mask(hist, img):
-	img = cv2.blur(img, (5, 5))
-	mask = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-	mask = cv2.calcBackProject([mask], [1, 2], hist, [0, 255, 0, 255], 1)
-	#mask = denoise(mask)
-	#mask = cv2.blur(mask, (5,5))
-	return mask
-
-def getContours(mask):
-	_, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	return contours
-
-def getHullDefects(contours):
-	defects = []
-	for cnt in contours:
-		hull = cv2.convexHull(cnt, returnPoints=False)
-		defects.append(cv2.convexityDefects(cnt, hull))
-	return defects
+from backproj import mask, getHistogram
+from contours import getContours, handleContours
+from convexHulls import getHullDefects, drawConvexHulls
 
 def startVideoFeed(cam_index, hist=None):
 
@@ -43,19 +17,8 @@ def startVideoFeed(cam_index, hist=None):
 		thresh_frame = mask(roi_hist, frame)
 		contours = getContours(thresh_frame)
 		contours = handleContours(contours)
-		for cnt in contours:
-			hull = cv2.convexHull(cnt,returnPoints = False)
-			defects = cv2.convexityDefects(cnt,hull)
-			for i in range(defects.shape[0]):
-				s,e,f,d = defects[i,0]
-				start = tuple(cnt[s][0])
-				end = tuple(cnt[e][0])
-				far = tuple(cnt[f][0])
-				cv2.line(frame, start,end,[0,255,0],2)
-				cv2.circle(frame,far,5,[0,0,255],-1)
-		#cv2.drawContours(frame, contours, -1, (0,255,0), 2)
-
-
+		hulls = getHullDefects(contours)
+		drawConvexHulls(frame, contours, hulls)
 		cv2.imshow('frame', frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
